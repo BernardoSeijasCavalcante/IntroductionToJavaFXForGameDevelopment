1db40f0c15bb8a54dff5c246fd253f339c36d66b
Bernardo <bernardo.cavalcante@edu.fecap.br>
2025-01-03 11:11:37 -0300

    The Game Loop

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,49 +1,45 @@
 package application;
 	
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage theStage) {
-		theStage.setTitle( "Canvas Example" ); // Dá um título à janela
-        
-	    Group root = new Group(); // Cria um agrupamento para nodes
-	    Scene theScene = new Scene( root );  //Instância a cena com o agrupamento como a raiz dos nodes 
-	    theStage.setScene( theScene ); //Coloca a cena no palco
-	        
-	    Canvas canvas = new Canvas( 400, 300 ); //Instância o canvas para alocação de textos, imagens, formas e afins
-	    root.getChildren().add( canvas ); //Adiciona o canvas como primeiro filho (nó ou node) da raiz
-	        
-	    GraphicsContext gc = canvas.getGraphicsContext2D(); //Instância um GraphicsContext para a criação e alocação de objetos no canvas. GraphicsContext é como um serviço que contém vários recursos gráficos
-	        
-	    gc.setFill( Color.RED );
-	    gc.setStroke( Color.BLACK );
-	    gc.setLineWidth(2);
-	    Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
-	    gc.setFont( theFont );
-	    gc.fillText( "Hello, World!", 60, 50 );
-	    gc.strokeText( "Hello, World!", 60, 50 );
-	    
-	    gc.setFill( Color.BLACK );
-	    gc.setStroke( Color.RED );
-	    gc.setLineWidth(2);
-	    Font theFont2 = Font.font( "Calibri", FontWeight.BOLD, 36 );
-	    gc.setFont( theFont2 );
-	    gc.fillText( "Hello, Nothing!", 90, 250 );
-	    gc.strokeText( "Hello, Nothing!", 90, 250 );
-	    
+		theStage.setTitle( "Timeline Example" );
+	    Group root = new Group();
+	    Scene theScene = new Scene( root );
+	    theStage.setScene( theScene );
+	    Canvas canvas = new Canvas( 512, 512 );
+	    root.getChildren().add( canvas );
+	    GraphicsContext gc = canvas.getGraphicsContext2D();
 	    Image earth = new Image( "earth.png" );
-	    gc.drawImage(earth, 150, 100, 100, 100);
+	    Image sun   = new Image( "sun.png" );
+	    Image space = new Image( "space.jpeg" );
+	    final long startNanoTime = System.nanoTime();
+	    new AnimationTimer()
+	    {
+	        public void handle(long currentNanoTime)
+	        {
+	            double t = (currentNanoTime - startNanoTime) / 1000000000.0; 
+	            System.out.println(t);
+	            double x = 231 + 128 * Math.cos(t);
+	            double y = 231 + 128 * Math.sin(t);
+	            // background image clears canvas 
+	            gc.clearRect(0,0,512,512);
+	            gc.drawImage( space, 0, 0 ,512,512);
+	            gc.drawImage( earth, x, y , 50, 50);
+	            gc.drawImage( sun, 206, 206, 100 , 100 );
+	            
+	        }
+	    }.start();
 	    
 	    theStage.show();
 	}
