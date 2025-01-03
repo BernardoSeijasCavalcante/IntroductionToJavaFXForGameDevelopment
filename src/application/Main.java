3d02ccb3ac2f85849cf21e39eca51d440fab0476
Bernardo <bernardo.cavalcante@edu.fecap.br>
2025-01-03 11:44:24 -0300

    Frame-Based Animation

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 	
+import animation.AnimatedImage;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
@@ -13,35 +14,45 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	@Override
 	public void start(Stage theStage) {
-		theStage.setTitle( "Timeline Example" );
-	    Group root = new Group();
-	    Scene theScene = new Scene( root );
-	    theStage.setScene( theScene );
-	    Canvas canvas = new Canvas( 512, 512 );
-	    root.getChildren().add( canvas );
-	    GraphicsContext gc = canvas.getGraphicsContext2D();
-	    Image earth = new Image( "earth.png" );
-	    Image sun   = new Image( "sun.png" );
-	    Image space = new Image( "space.jpeg" );
-	    final long startNanoTime = System.nanoTime();
-	    new AnimationTimer()
-	    {
-	        public void handle(long currentNanoTime)
-	        {
-	            double t = (currentNanoTime - startNanoTime) / 1000000000.0; 
-	            System.out.println(t);
-	            double x = 231 + 128 * Math.cos(t);
-	            double y = 231 + 128 * Math.sin(t);
-	            // background image clears canvas 
-	            gc.clearRect(0,0,512,512);
-	            gc.drawImage( space, 0, 0 ,512,512);
-	            gc.drawImage( earth, x, y , 50, 50);
-	            gc.drawImage( sun, 206, 206, 100 , 100 );
-	            
-	        }
-	    }.start();
-	    
-	    theStage.show();
+		
+		theStage.setTitle("Frame-Based Animation");
+		
+		Group root = new Group();
+		Scene scene = new Scene(root);
+		theStage.setScene(scene);
+		
+		Canvas canvas = new Canvas(520,520);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+
+		root.getChildren().add(canvas);
+		
+		AnimatedImage ufo = new AnimatedImage();
+		Image[] imageArray = new Image[6];
+		
+		imageArray[1] = new Image( "earth.png" );
+		imageArray[2] = new Image( "space.jpeg" );
+		imageArray[3] = new Image( "sun.png" );
+		
+		ufo.frames = imageArray;
+		ufo.duration = 0.100;
+		
+		final long startNanoTime = System.nanoTime();
+		
+		
+		new AnimationTimer(){
+			
+			public void handle(long currentNanoTime) {
+				double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+				
+				gc.drawImage( ufo.getFrame(t), 0, 25 ); 
+				
+				System.out.println(t);
+				
+			}
+			
+		}.start();
+		
+		theStage.show();
 	}
 	
 	public static void main(String[] args) {
